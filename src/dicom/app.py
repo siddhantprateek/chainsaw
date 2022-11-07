65ca4e2a9a653b9ac555f96f140341865e6edb62
Nikhita <bnikhita2003@gmail.com>
2022-11-07 22:31:13 +0530

    updated app

--- src/dicom/app.py
+++ src/dicom/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
-
+from src.dicom import jsondata
 app = Flask(__name__)
 
 @app.route("/")
 def home():
   return "Hello World!"
+
+@app.route("/data", methods=['GET'])
+def datas():
+  return jsondata
 
 if __name__ == "__main__":
   app.run()
