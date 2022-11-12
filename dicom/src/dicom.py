5f48f17eb21a1235aac8b9d83a31352dc6082733
Siddhant Prateek <siddhantprateek@gmail.com>
2022-11-13 01:50:38 +0530

    dicom file metadata extractor

--- dicom/src/dicom.py
+++ dicom/src/dicom.py
@@ -2,16 +2,18 @@ import pydicom
 
 # dataset = pydicom.dcmread("./src/assets/CT-MONO2-16-ankle.dcm")
 
-# print(dataset)
+# @desp: process dicom file and extract metadata
+# @returns: dicom file metadata
 def process_dicom(dicom_src: str):
     dicom_data = pydicom.dcmread(dicom_src)
     return dicom_data
 
 
-# .
-dataset = process_dicom("./train/5.dcm")
+if __name__ == '__main__':
+    dataset = process_dicom("./train/5.dcm")
 
-jsondata = {
+
+    jsondata = {
     "Manufacturer": str(dataset.Manufacturer),
     "SeriesTime": str(dataset.SeriesTime),
     "PatientName": str(dataset.PatientName),
@@ -23,5 +25,4 @@ jsondata = {
     "SeriesNumber": dataset.SeriesNumber
     }
     
-rawdata = dict(dataset)
-# print()
+    rawdata = dict(dataset)
