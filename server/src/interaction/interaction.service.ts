75b65998920b947e36915c13ecd4306d03335cd3
Siddhant Prateek <siddhantprateek@gmail.com>
2022-11-13 13:03:10 +0530

    web3 interaction web3 services

--- server/src/interaction/interaction.service.ts
+++ server/src/interaction/interaction.service.ts
@@ -1,6 +1,34 @@
 import { Injectable, BadRequestException } from "@nestjs/common";
 import truffleContract from 'truffle-contract';
 import * as DicomArtifacts from '../../../client/src/build/contracts/DiacomContract.json';
-
+import web3 from "../config/utility";
 
 const DicomContract = truffleContract(DicomArtifacts);
+
+@Injectable()
+export class DicomService {
+
+    async getBalance(account: string): Promise<string> {
+        try {
+            const instance = await DicomContract.deployed();
+            const value = await instance.getBalance.call(account, { from: account });
+            const balance = value.toString();
+
+            return JSON.stringify({
+                address: account,
+                balance: balance
+            })
+        } catch (error) {
+            console.log(error);
+            throw new BadRequestException({ description: 'Unable to get balance of account: '+ account});
+        }
+    }
+
+
+    async getContractAddress(): Promise<string> {
+        const instance = await DicomContract.deployed();
+        return JSON.stringify({
+            contractAddress: instance.address
+        })
+    }
+}
