b05ad4e745c645952e6ae05cf86fdbf2a3a28ad5
Siddhant Prateek <siddhantprateek@gmail.com>
2022-11-15 13:02:15 +0530

    interaction controller

--- server/src/interaction/interaction.controller.ts
+++ server/src/interaction/interaction.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get, Post, Body, ValidationPipe, UsePipes, Param } from '@nestjs/common';
-
+// import { InteractionService } from './interaction.service';
 
 
 @Controller('/')
 export class InteractionController {
-
+    // constructor(private readonly appService: InteractionService);
     
 }
