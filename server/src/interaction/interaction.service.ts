96a5e704ccc865c4c3d60926788657c01ab542a7
Nikhita <bnikhita2003@gmail.com>
2022-11-18 20:06:16 +0530

    interaction web3

--- server/src/interaction/interaction.service.ts
+++ server/src/interaction/interaction.service.ts
@@ -2,8 +2,10 @@ import { Injectable, BadRequestException } from "@nestjs/common";
 import truffleContract from 'truffle-contract';
 import * as DicomArtifacts from '../../../client/src/build/contracts/DiacomContract.json';
 import web3 from "../config/utility";
+import { TokenTransferDto } from './dto/token-transfer.dto';
 
 const DicomContract = truffleContract(DicomArtifacts);
+DicomContract.setProvider(web3.currentProvider);
 
 @Injectable()
 export class DicomService {
@@ -31,4 +33,41 @@ export class DicomService {
             contractAddress: instance.address
         })
     }
+
+    async getBalanceInEth(account: string): Promise<string> {
+        try {
+          const instance = await DicomContract.deployed();
+          const value = await instance.getBalanceInEth.call(account, { from: account });
+          const balance = value.toString();
+    
+          return JSON.stringify({
+            address: account,
+            balance: balance
+          });
+        } catch (error) {
+          console.log(error)
+          throw new BadRequestException({ description: 'Unable to get balance of account: '+ account});
+        }
+      }
+
+      async sendSignedTransaction(tokenTransferDto: TokenTransferDto): Promise<string> {
+        const { tx } = tokenTransferDto;
+        let resp;
+        try {
+          await web3.eth.sendSignedTransaction(tx).on('receipt', receipt => {
+            const { transactionHash, status, to, blockNumber } = receipt;
+            resp = {
+              transactionHash: transactionHash,
+              status: status,
+              to: to,
+              blockNumber: blockNumber
+            }
+          });
+        } catch (error) {
+          console.log(error)
+          throw new BadRequestException({ description: error.message });
+        }
+        return resp;
+      }
+
 }
