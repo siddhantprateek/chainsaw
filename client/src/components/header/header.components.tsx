bace925bc9dc65e4b89aa50f133ae12577e15234
Nikhita <bnikhita2003@gmail.com>
2022-11-19 13:03:15 +0530

    header component

--- client/src/components/header/header.components.tsx
+++ client/src/components/header/header.components.tsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './header.styles.css';
+
+interface INAV {
+  option: string,
+  route: string
+};
+
+
+const NAV_OPTION = [
+  {
+    option: "Solution",
+    route: "/solution"
+  },
+  {
+    option: "Resource",
+    route: "/route"
+  },
+  {
+    option: "Product",
+    route: "/product"
+  }
+];
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="left">
+        <div className='logo'>
+          <Link to="/">
+            <h2>Chain<span className='logo-green'>saw</span></h2>
+          </Link>
+        </div>
+      </div>
+      <div className="mid">
+        <ul>
+          { 
+            NAV_OPTION.map((item: INAV) =>(
+            <li>
+              <Link to={item.route}>
+                {item.option}
+              </Link>
+            </li>
+            )) 
+          }
+        </ul>
+      </div>
+      <div className="right">
+        <div className="connect-btn">
+          Connect
+        </div>
+      </div>
+    </header>
   )
 }
 
