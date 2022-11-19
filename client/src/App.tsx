2996d8c9719cb6ebb98ece0ace51fef58cf7207c
Nikhita <bnikhita2003@gmail.com>
2022-11-19 13:04:43 +0530

    client routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Home, Dashboard } from './pages';
+import { Routes, Route } from 'react-router-dom';
+import { Header, Footer } from './components';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Heloo</h1>
+      <Header />
+      <Routes>
+        <Route index element={<Home/>} />
+        <Route path='dashboard' element={<Dashboard/>} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
