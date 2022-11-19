b5c1c0150e3c046cbcbb55c7bf878bbc38bcd6bd
Nikhita <bnikhita2003@gmail.com>
2022-11-19 13:48:27 +0530

    functions for conversion

--- dicom/src/dicomconversion.py
+++ dicom/src/dicomconversion.py
@@ -11,3 +11,30 @@ final_image = Image.fromarray(scaled_image)
 final_image.show()
 final_image.save('image.png')
 
+def get_names(path):
+    names = []
+    for root, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            _, ext = os.path.splitext(filename)
+            if ext in ['.dcm']:
+                names.append(filename)
+    
+    return names
+
+def convert_dcm_jpg(name):
+    
+    im = pydicom.dcmread('Database/'+name)
+
+    im = im.pixel_array.astype(float)
+
+    rescaled_image = (np.maximum(im,0)/im.max())*255 # float pixels
+    final_image = np.uint8(rescaled_image) # integers pixels
+
+    final_image = Image.fromarray(final_image)
+
+    return final_image
+
+names = get_names('Database')
+for name in names:
+    image = convert_dcm_jpg(name)
+    image.save(name+'.png')
