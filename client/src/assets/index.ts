394a48ca645b8580a550f1381cbfac5ddfca53ae
Siddhant Prateek <siddhantprateek@gmail.com>
2022-11-21 20:29:40 +0530

    default exports fo components & pages

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,1 +1,4 @@
 export { default as LAYDOWNSVG } from './laydown.svg';
+export { default as ONCHAIN } from './onchain.png';
+export { default as SECURITYIMG } from './securityimg.png';
+export { default as LOCK } from './lock.png';
