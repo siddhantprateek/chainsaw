2aef3b715a0fb587d47577fca6b8dcb78469b345
Siddhant Prateek <siddhantprateek@gmail.com>
2022-11-22 00:21:24 +0530

    graphql apollo client provider added

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+
+// @dev uri: http://localhost:4000
+// @production: https://
+// GraphQL Provider 
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4000',
+//   cache: new InMemoryCache(),
+// })
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {/* <ApolloProvider client={client}> */}
+        <App />
+      {/* </ApolloProvider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
