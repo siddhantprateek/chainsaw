f1c3e252a833499faa253006e1cdeecff0a149cd
Nikhita <bnikhita2003@gmail.com>
2022-11-22 13:32:30 +0530

    server side graphql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,18 @@ import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [CacheModule.register(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [
     {
