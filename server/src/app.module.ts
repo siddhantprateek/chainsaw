4149d5d827c4fb4003b1f9842eccdfcbb59db4c5
Nikhita <bnikhita2003@gmail.com>
2022-11-22 13:39:56 +0530

    fixed bugs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,9 @@ import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [CacheModule.register(),
