9ffe054d820278ef6d700dcc3f20720a86b6164e
Nikhita <bnikhita2003@gmail.com>
2022-11-23 20:49:54 +0530

    exported contents for dashboard

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,3 +2,7 @@ export { default as Header } from './header/header.components';
 export { default as Footer } from './footer/footer.components';
 export { default as Hero } from './hero/hero.components';
 export { default as FeatureCard } from './featurecard/featurecard.components';
+export { default as Sidebar } from './sidebar/sidebar.components';
+export { default as ReportAnalysis } from './reportanalysis/reportAnalysis.components';
+export { default as Reportwide } from './reportwide/reportwide.components';
+export { default as Reportsquare } from './reportsquare/reportsquare.components';
