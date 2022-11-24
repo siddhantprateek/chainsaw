3ba9396b67c9a7f5f904f2ce02da8bf6346bb078
Nikhita <bnikhita2003@gmail.com>
2022-11-24 15:42:06 +0530

    reportanalysis updated

--- client/src/components/reportanalysis/reportAnalysis.components.tsx
+++ client/src/components/reportanalysis/reportAnalysis.components.tsx
@@ -2,14 +2,27 @@ import React from 'react'
 import './reportAnalysis.styles.css'
 import { Ellipse } from '../../assets'
 import { Reportwide, Reportsquare } from '../../components'
+
+const UserProfile = () => (
+<div className="title-items">
+  <img src={Ellipse} className='item-image' alt="" />
+</div>
+);
+
+
 const ReportAnalysis = () => {
   return (
     <div className='report-analysis'>
+
       <div className="report-text">
         <h2 className="report-title">Report Analysis</h2>
-        <div className="title-items">
-          <img src={Ellipse} className='item-image' alt="" />
-          <div className="sidetext">Granted Access</div>
+        <div className="grant-section">
+          <div className="granted-user">
+            <UserProfile />
+            <UserProfile />
+            <UserProfile />
+          </div>
+          <p className="sidetext">Granted Access</p>
         </div>
       </div>
       <div className='cards'>
